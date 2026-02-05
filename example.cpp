29b4433c714aa35762ef388d36df141cce1b7c33
KristiDvD <99163814+sobrak@users.noreply.github.com>
2026-02-05 12:47:47 +0300

    Add files via upload

--- example.cpp
+++ example.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "eztreamlib.h"
 
-int main() {
-    std::cout << "this is the simple example how to use this lib" << std::endl;
+void write() {
+    const char* TestFile[] = {"test.txt", "Hello again))", nullptr};
+    Writefile(TestFile);
+}
+
+void read() {
     const char* TestFile[] = {"test.txt", nullptr};
     Readfile(TestFile);
+}
+
+int main() {
+    std::cout << "this is the simple example how to use this lib" << std::endl;
+    read();
+    write();
+    read();
     return 0;
 }
